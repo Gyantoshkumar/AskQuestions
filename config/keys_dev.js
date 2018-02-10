106e2a3214e15180b7bb64771882b84dbeae7325
Gyantosh kumar <gyantosh395@gmail.com>
2018-02-10 18:33:02 +0530

    hopeItworks4

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports={
+	mongoURI:'mongodb://gyan395:Gyantosh@ds161950.mlab.com:61950/storybooks-dev',
+	googleClientID:'299973017793-h263o4djo57slf3f39b7bkvpitu16ior.apps.googleusercontent.com',
+	googleClientSecret:'ZWzd_EMKl5ufKDQ0rh7bvHOm'
+}
